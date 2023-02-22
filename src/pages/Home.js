75623ca14956f3cc8174c999f8dc8737118d9924
Sasha Marshall <sashamarshalldesigns@gmail.com>
2023-02-22 10:05:12 -0800

    styling changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Photo from '../components/layout/Photo'
 
 const Home = () => {
   return (
-    <div>
+    <div className='h-screen lg:mx-40 xl:mx-80'>
         <h1 className='text-5xl font-bold'>Welcome, click below to generate a random photo.</h1>
         <Photo/>
     </div>
